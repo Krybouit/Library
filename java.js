d11572496bcb391ada5f04b0d9f7b5b4ddf63ff0
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-02-17 20:57:49 -0500

    Add header title with button

---
+++ java.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+
+
+function Book(title, author, pages, read) {  
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    
+}
+
+
+
+
+
+
+
+//const book1 = new Book('harry potter,', ' jk,', ' 43,', ' not read');
+
+
