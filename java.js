570dabdb4cf8037d113176cd74f18737aabb5836
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-02-25 19:32:17 -0500

    Add cancel and add button to book form
    *adds styling to both buttons
    *adds styling to input fields

--- java.js
+++ java.js
@@ -1,12 +1,15 @@
 const openModal = document.querySelector(".new-book");
 const modal = document.querySelector(".modal");
+const cancelBtn = document.getElementById("cancel-button");
 const myLibrary = [];
 
 openModal.addEventListener("click", () => {
     modal.showModal();
 })
 
-
+cancelBtn.addEventListener("click", () => {
+    modal.close();
+})
 
 function Book(title, author, pages, read) {  
     this.title = title;
