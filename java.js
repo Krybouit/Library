417a583fba0f264e804658385d073bb37db928cf
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-02-27 18:54:23 -0500

    Add function to that takes user input
    *the function takes user input and pushes it to
    an array.

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 const openModal = document.querySelector(".new-book");
 const modal = document.querySelector(".modal");
 const cancelBtn = document.getElementById("cancel-button");
+const submitBtn = document.getElementById("submit");
 const myLibrary = [];
 
 openModal.addEventListener("click", () => {
@@ -17,12 +18,22 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
-
-function addBookToLibrary() {
-
-}
-    
-
+ 
+submitBtn.addEventListener("click", function addBookToLibrary(e) {
+    e.preventDefault()
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("checkbox").checked;
+    if (read === true) {
+        read = "yes";
+    } else {
+        read = "no";
+    }
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+})
 
 
 
