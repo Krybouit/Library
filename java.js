d06a2239c92727722fae99621fbad15751cf5ae5
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-03-06 18:54:30 -0500

    Add new function for form
    Displays book details from the user's input on the
    HTML.

--- java.js
+++ java.js
@@ -2,7 +2,10 @@ const openModal = document.querySelector(".new-book");
 const modal = document.querySelector(".modal");
 const cancelBtn = document.getElementById("cancel-button");
 const form = document.getElementById("form");
-const myLibrary = [];
+const bookList = document.getElementById("book-list");
+let myLibrary = [];
+
+
 
 openModal.addEventListener("click", () => {
     modal.showModal();
@@ -12,7 +15,6 @@ cancelBtn.addEventListener("click", () => {
     modal.close();
     form.reset();
 })
-
 
 
 function Book(title, author, pages, read) {  
@@ -35,10 +37,31 @@ form.addEventListener("submit", function addBookToLibrary(e) {
     }
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    console.log(myLibrary);
     modal.close();
     form.reset();
+    bookLoops();
+   
 })
+
+
+function bookLoops() {
+        const bookBox = document.createElement("div");
+        bookList.appendChild(bookBox);
+        bookBox.setAttribute("class","book-container");
+        bookBox.innerHTML =
+            "Title: " + myLibrary[0].title +
+            " Author: " + myLibrary[0].author +
+            " Pages: " + myLibrary[0].pages +
+            " Read: " + myLibrary[0].read;
+        clearLibrary();
+        console.log(myLibrary);
+    }
+
+function clearLibrary() {
+    myLibrary = [];
+}
+
+
 
 
 
